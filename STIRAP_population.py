92415bec02908dd3f2bb1ca43d7749db68cabd2e
Nakarin Jayjong <93529299+Nakarin315@users.noreply.github.com>
2022-01-17 14:52:52 +0800

    Update STIRAP_population.py

--- STIRAP_population.py
+++ STIRAP_population.py
@@ -1,8 +1,10 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-from scipy.integrate import complex_ode
 
+
+###############################################################################
+## Define odeint for a complex number
 def odeintz(func, z0, t, **kwargs):
     """An odeint-like function for complex valued differential equations."""
 
@@ -33,13 +35,11 @@ def odeintz(func, z0, t, **kwargs):
     else:
         z = result.view(np.complex128)
         return z
+###############################################################################
 
 
-
-# Solve y1'(t) = y1+2*y2;  y2'(t) = -5*y1-y2; y1(0) =2; y2(0)=1
-# x0=0.55
-# sigma_Omega=0.72;
-
+###############################################################################
+## Define pulsed Rabi frequencies
 sigma_Omega=0.4;
 x0=-0.73*sigma_Omega;
 def dydt(y0, t):
@@ -56,18 +56,9 @@ t=np.linspace(-2, 2, 1000)
 Omega_S_t =5*np.pi*np.exp(-0.5*((t-x0)/sigma_Omega)**2)/(sigma_Omega*np.sqrt(2*np.pi));
 Omega_P_t =5*np.pi*np.exp(-0.5*((t+x0)/sigma_Omega)**2)/(sigma_Omega*np.sqrt(2*np.pi));
 
-
-
-
-
-# A_matrix = -1j*np.array([[0,Omega_P/2,0],
-#                 [Omega_P/2,Delta,Omega_S/2],
-#                 [0,Omega_S/2,delta]])
-
-#t = np.linspace(0, 20, 100)
 y0 = np.array([1,0,0])
 
-MA = odeintz(dydt, y0, t, rtol=1e-10,atol=1e-9,hmax=1e-5)
+MA = odeintz(dydt, y0, t, rtol=1e-10,atol=1e-10)
 y1 = MA[:,0]
 y2 = MA[:,1]
 y3 = MA[:,2]
@@ -75,12 +66,16 @@ y1 = np.real(np.conjugate(y1)*y1)
 y2 = np.real(np.conjugate(y2)*y2)
 y3 = np.real(np.conjugate(y3)*y3)
 
+
+###############################################################################
+## Plot pulsed Rabi frequencies
 fig, (ax1, ax2) = plt.subplots(2)
 ax1.plot(t,Omega_S_t*(sigma_Omega*np.sqrt(2*np.pi))/(5*np.pi),'--', label='S');
 ax1.plot(t,Omega_P_t*(sigma_Omega*np.sqrt(2*np.pi))/(5*np.pi), label='P');
 ax1.legend(loc=2, prop={'size': 15})
 ax1.set(xlabel='Time', ylabel='Pulse')
 
+## Plot populations
 ax2.plot(t,y1,'g--', label='1')
 ax2.plot(t,y2,'r', label='2')
 ax2.plot(t,y3,'b', label='3')
